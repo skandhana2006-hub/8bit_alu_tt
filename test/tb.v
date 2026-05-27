094fd903a3fe6a003d25e0fcbd1afd29667a007f
skandhana2006-hub <skandhana2006@gmail.com>
2026-05-27 15:32:19 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,44 @@
-`default_nettype none
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+module tb;
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+reg [7:0] ui_in;
+reg [7:0] uio_in;
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+wire [7:0] uo_out;
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+reg clk;
+reg rst_n;
+reg ena;
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+tt_um_8bit_alu dut (
+    .ui_in(ui_in),
+    .uio_in(uio_in),
+    .uo_out(uo_out),
+    .ena(ena),
+    .clk(clk),
+    .rst_n(rst_n)
+);
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+initial begin
+
+    clk = 0;
+    forever #5 clk = ~clk;
+
+end
+
+initial begin
+
+    ena = 1;
+    rst_n = 1;
+
+    ui_in = 8'd5;
+    uio_in = 8'd3;
+
+    #20;
+
+    $finish;
+
+end
 
 endmodule
